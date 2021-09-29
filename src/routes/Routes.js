c5ec1d20396da4ec598a5b5e7aa5ff947841effd
AndreLuizGomesPereira <andregomesenator@gmail.com>
2021-09-28 23:22:20 -0300

    Implementado a página de detalhes

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,31 +1,43 @@
-import React from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import ErrorPage from '../pages/ErrorPage/ErrorPage'
-import HomePage from '../pages/HomePage/HomePage'
-import PokedexPage from '../pages/PokedexPage/PokedexPage'
-import PokemonListPage from '../pages/PokemonListPage/PokemonListPage'
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import ErrorPage from '../pages/ErrorPage/ErrorPage';
+import HomePage from '../pages/HomePage/HomePage';
+import PokedexPage from '../pages/PokedexPage/PokedexPage';
+import PokemonListPage from '../pages/PokemonListPage/PokemonListPage';
+import PokemonDetailsPage from '../pages/PokemonDetailsPage/PokemonDetailsPage';
 
 const Routes = () => {
     return (
         <>
             <BrowserRouter>
+
                 <Switch>
+
                     <Route exact path={"/"}>
                         <HomePage />
                     </Route>
+
                     <Route exact path={"/pokemon-list"}>
                         <PokemonListPage />
                     </Route>
-                    <Route exact path={"/pokedex"}>
+
+                    <Route path={"/pokedex"}>
                         <PokedexPage />
                     </Route>
+
+                    <Route path={"/pokemon-details"}>
+                        <PokemonDetailsPage />
+                    </Route>
+
                     <Route>
                         <ErrorPage />
                     </Route>
+
                 </Switch>
+
             </BrowserRouter>
         </>
     )
 }
 
-export default Routes
+export default Routes;
